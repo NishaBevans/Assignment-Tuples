063ad2e052f50da825f8e4bb63d6486da87e2281
Nisha Bevans <nisha.chavez@yahoo.com>
2024-10-05 11:48:32 -0500

    Updated commit

--- PythonDataStructure.py
+++ PythonDataStructure.py
@@ -1,3 +1,11 @@
+#Task 1: Library System Enhancement Sharpen your skills in managing and modifying data within tuples and lists.
+#You are maintaining a library system where each book is stored as a tuple within a list. 
+#Your task is to update this system by adding new books and ensuring no duplicates.
+#existing Library Data:
+#library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+#-Add functionality to insert new books into `library`. Ensure that adding a duplicate book is handled appropriately.
+
+
 def add_book(library, book):
     if book in library:
         print(f"The book '{book[0]}' by {book[1]} already exists in the library.")
--- TupleMasteryinPython.py
+++ TupleMasteryinPython.py
@@ -5,9 +5,6 @@
 #"Itinerary 1: Alice - From New York to London
 #Itinerary 2: Bob - From Tokyo to San Francisco"
 
-
-
-
 def format_itineraries(itineraries):
     return "\n".join(
         f"Itinerary {i + 1}: {name} - From {origin} to {destination}"
